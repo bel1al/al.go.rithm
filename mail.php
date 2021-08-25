67bb70695ff4bc5132d2346124986504fa358790
bel1al <skywokerua@gmail.com>
2021-08-25 21:55:07 +0300

    try to fix netlify

--- mail.php
+++ mail.php
@@ -1,13 +1,15 @@
 <?php
 //get data from form  
+if(isset($_POST['submit'])){
 $name = $_POST['name'];
 $email= $_POST['email_addr'];
 $message= $_POST['old'];
 
-$mailTo = "skywokerua@email.com";
+$mailTo = "skywokeruarad@gmail.com";
 $headers =  "From: ".$email;
 
 mail($mailTo, $name, $message, $headers);
 //redirect
 header("Location:details.html");
+}
 ?>

e949fd961e17ac49dc50d93fcf00d81b47627ebe
bel1al <skywokerua@gmail.com>
2021-08-25 21:23:25 +0300

    create modal window and email sender

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById('modalContact');
+
+const btn = document.getElementById('showContact');
+
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};

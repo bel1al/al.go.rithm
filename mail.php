1b7574d626c5ae5cfc153e40fdcd83a04cb410b4
bel1al <skywokerua@gmail.com>
2021-08-25 21:45:24 +0300

    try to fix netlify

--- mail.php
+++ mail.php
@@ -4,9 +4,11 @@ $name = $_POST['name'];
 $email= $_POST['email_addr'];
 $message= $_POST['old'];
 $to = "skywokerua@mail.com";
-$subject = "Mail From website";
+
+$subject = "Mail From algorithm";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+
+$headers = "From: noreply@algorithm.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
